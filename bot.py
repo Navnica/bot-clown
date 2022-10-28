7e789a07f2c7f6f2c39fbb0f4b14920136368900
TruEnot <truenot@vivaldi.net>
2022-10-28 23:53:46 +0300

    update userworker.py, bot.py

--- bot.py
+++ bot.py
@@ -11,9 +11,10 @@ bot = telebot.TeleBot(config['token'])
 
 
 @bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if not userworker.user_registered(message.from_user.id):
-        userworker.register_user(message.from_user.__dict__)
+def get_text_messages(message) -> None:
+    user_worker = userworker.UserWorker(message.from_user.id)
+    if not user_worker.user_registered():
+        user_worker.register_user()
 
     bot.send_message(chat_id=message.from_user.id, text='👉👈🥺', reply_markup=keyboards.get_select_category_markup())
     print(cowsay.get_output_string(random.choice(cowsay.char_names), f'{message.from_user.id} : {message.text}'))
@@ -21,24 +22,25 @@ def get_text_messages(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    userworker.set_user_message(call.from_user.id, call.message.id)
-    user = json.load(open('users.json', encoding='utf-8'))[str(call.from_user.id)]
+    user_worker = userworker.UserWorker(call.from_user.id)
+    user_worker.set_user_message(call.message.id)
 
     if call.data == 'change_category':
-        bot.edit_message_text(chat_id=call.from_user.id, text='🫵🏻🤡', message_id=user['message_id'],
+        bot.edit_message_text(chat_id=call.from_user.id, text='🫵🏻🤡', message_id=user_worker.get_message_id(),
                               reply_markup=keyboards.get_select_category_markup())
 
     if call.data in config['category']:
-        userworker.set_user_category(user_id=call.from_user.id, category=call.data)
-        bot.edit_message_text(text='👉👈🥺', chat_id=call.from_user.id, message_id=user['message_id'],
+        user_worker.set_user_category(category=call.data)
+        bot.edit_message_text(text='👉👈🥺', chat_id=call.from_user.id, message_id=user_worker.get_message_id(),
                               reply_markup=keyboards.get_default_markup())
 
     if call.data == 'drop':
-        anek = anekmanage.get_anek_with_category(user['category'])
-        bot.edit_message_text(text=anek, chat_id=call.from_user.id, message_id=user['message_id'],
+        anek = anekmanage.get_anek_with_category(user_worker.get_category())
+        bot.edit_message_text(text=anek, chat_id=call.from_user.id, message_id=user_worker.get_message_id(),
                               reply_markup=keyboards.get_default_markup())
 
     print(cowsay.get_output_string(random.choice(cowsay.char_names), f'{call.from_user.id} : {call.data}'))
+
 
 def start_polling():
     bot.infinity_polling()
--- userworker.py
+++ userworker.py
@@ -1,29 +1,31 @@
 import json
 
 
-def user_registered(user_id: str) -> bool:
-    return str(user_id) in json.load(open('users.json', encoding='utf-8'))
+class UserWorker:
+    def __init__(self, user_id) -> None:
+        self.user_id: str = str(user_id)
+        self.users: dict = json.load(open('users.json', encoding='utf-8'))
 
+    def user_registered(self) -> bool:
+        return self.user_id in self.users
 
-def register_user(user: dict) -> None:
-    users: dict = json.load(open('users.json', encoding='utf-8'))
-    users[str(user['id'])] = {}
-    json.dump(users, open('users.json', 'w', encoding='utf-8'))
+    def get_message_id(self) -> int:
+        return self.users[self.user_id]['message_id']
 
+    def get_category(self) -> str:
+        return self.users[self.user_id]['category']
 
-def set_user_category(user_id: str, category: str) -> None:
-    users: dict = json.load(open('users.json', encoding='utf-8'))
-    users[str(user_id)]['category'] = category
-    json.dump(
-        users,
-        open('users.json', 'w', encoding='utf-8')
-    )
+    def register_user(self) -> None:
+        self.users[self.user_id] = {}
+        self.dump_updates()
 
+    def set_user_category(self, category) -> None:
+        self.users[self.user_id]['category'] = category
+        self.dump_updates()
 
-def set_user_message(user_id: str, message_id: str) -> None:
-    users: dict = json.load(open('users.json', encoding='utf-8'))
-    users[str(user_id)]['message_id'] = str(message_id)
-    json.dump(
-        users,
-        open('users.json', 'w')
-    )
+    def set_user_message(self, message_id) -> None:
+        self.users[self.user_id]['message_id'] = message_id
+        self.dump_updates()
+
+    def dump_updates(self) -> None:
+        json.dump(self.users, open('users.json', 'w', encoding='utf-8'))
